e077c637664229c3cd3e52711063ee217f3091f4
Maciej Rościszewski <maciej.rosciszewski@progrupa.com>
2024-12-11 23:54:04 +0100

    Add try-catch  API

--- query/user.js
+++ query/user.js
@@ -11,13 +11,21 @@ export const allusers = async () => {
 };
 
 export const register_system_user = async (username, hashedPassword) => {
-  const [user] = await pool.query(
-    "INSERT INTO system_users (first_name, last_name, email, password_hash, system_user_role, is_active, created_at, updated_at) VALUES (?, ?, ?, ?, 'developer', true, NOW(), NOW())",
-    ["Maciej", "Rościszewski", username, hashedPassword]);
+  try{
+    await pool.query(
+      "INSERT INTO system_users (first_name, last_name, email, password_hash, system_user_role, is_active, created_at, updated_at) VALUES (?, ?, ?, ?, 'developer', true, NOW(), NOW())",
+      ["Maciej", "Rościszewski", username, hashedPassword]);
 
     return {
-      message: "Użytkownik dodany",
+      message: "Użytkownik dodany pomyślnie",
       error: false,
-      data: user
+      data: null
     };
+  } catch (error) {
+    return {
+      message: "Wystąpił błąd podczas dodawania użytkownika",
+      error: true,
+      data: null
+    };
+  }
 }

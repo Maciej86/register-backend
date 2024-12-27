e894da0609f9fd6e9e238b480e910d7aeb52bcbb
Maciej Rościszewski <maciej.rosciszewski@progrupa.com>
2024-12-27 11:52:52 +0100

    Edit query create user

--- query/create_user_query.js
+++ query/create_user_query.js
@@ -3,7 +3,7 @@ import { pool } from "../db.js";
 export const create_user_personal = async (first_name, last_name, email, hashedPassword, verificationToken) => {
   try{
     const result = await pool.query(
-      "INSERT INTO personal_accounts (first_name, last_name, email, password_hash, created_at, updated_at, is_verified, verification_token) VALUES (?, ?, ?, ?, NOW(), NOW(), false, ?)",
+      "INSERT INTO personal_accounts (first_name, last_name, email, password, created_at, updated_at, is_verified, verification_token) VALUES (?, ?, ?, ?, NOW(), NOW(), false, ?)",
       [first_name, last_name, email, hashedPassword, verificationToken]);
 
     return {

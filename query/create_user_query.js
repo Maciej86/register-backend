cf5d4e18bac49faac9002cf90e26a32711fae4a2
Maciej Rościszewski <maciej.rosciszewski@progrupa.com>
2024-12-29 23:27:34 +0100

    Create account - info server

--- query/create_user_query.js
+++ query/create_user_query.js
@@ -7,13 +7,13 @@ export const create_user_personal = async (first_name, last_name, email, hashedP
       [first_name, last_name, email, hashedPassword, verificationToken]);
 
     return {
-      message: "Użytkownik dodany pomyślnie",
+      message: "server.create_account_ok",
       error: false,
       data: result
     };
   } catch (error) {
     return {
-      message: "Wystąpił błąd podczas dodawania użytkownika",
+      message: "server.create_account_error",
       error: true,
       data: error
     };

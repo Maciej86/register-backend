41874bba1819cd360f717521e84e7934502f3527
Maciej Rościszewski <maciej.rosciszewski@progrupa.com>
2025-01-04 22:00:56 +0100

    Delete comment

--- query/create_user_query.js
+++ query/create_user_query.js
@@ -1,42 +1,36 @@
 import { pool } from "../db.js";
 
 export const create_user_personal = async (first_name, last_name, email, hashedPassword, verificationToken) => {
-  // try{
-  //   const [rows] = await pool.query(
-  //     "SELECT email FROM personal_accounts WHERE email = ?",
-  //     [email]
-  //   );
+  try{
+    const [rows] = await pool.query(
+      "SELECT email FROM personal_accounts WHERE email = ?",
+      [email]
+    );
 
-  //   if (rows.length > 0) {
-  //     return {
-  //       message: "server.email_already_exists",
-  //       error: true,
-  //       data: null
-  //     };
-  //   }
+    if (rows.length > 0) {
+      return {
+        message: "server.email_already_exists",
+        error: true,
+        data: null
+      };
+    }
 
-  //   const result = await pool.query(
-  //     "INSERT INTO personal_accounts (first_name, last_name, email, password, created_at, updated_at, is_verified, verification_token) VALUES (?, ?, ?, ?, NOW(), NOW(), false, ?)",
-  //     [first_name, last_name, email, hashedPassword, verificationToken]);
+    const result = await pool.query(
+      "INSERT INTO personal_accounts (first_name, last_name, email, password, created_at, updated_at, is_verified, verification_token) VALUES (?, ?, ?, ?, NOW(), NOW(), false, ?)",
+      [first_name, last_name, email, hashedPassword, verificationToken]);
 
-  //   return {
-  //     message: "server.create_account_ok",
-  //     error: false,
-  //     data: result
-  //   };
-  // } catch (error) {
-  //   return {
-  //     message: "server.create_account_error",
-  //     error: true,
-  //     data: error
-  //   };
-  // }
-
-  return {
-    message: "server.create_account_ok",
-    error: false,
-    data: null
-  };
+    return {
+      message: "server.create_account_ok",
+      error: false,
+      data: result
+    };
+  } catch (error) {
+    return {
+      message: "server.create_account_error",
+      error: true,
+      data: error
+    };
+  }
 };
 
 export const verify_account = async (token) => {

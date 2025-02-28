9d526094cf18372df3da6cfbb22725287d1e73f8
Maciej Rościszewski <maciej.rosciszewski@progrupa.com>
2025-02-28 21:17:03 +0100

    Add save child company

--- routes/company_api.js
+++ routes/company_api.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { company_accountants } from "../query/company_query.js";
+import { add_company, company_accountants } from "../query/company_query.js";
 import { authenticateToken } from "../authorization/authenticateToken.js";
 import { authorizeCompanyAccess } from "../authorization/authorizeCompanyAccess.js";
 
@@ -8,6 +8,13 @@ const router = express.Router();
 router.get("/company_accountants", authenticateToken, authorizeCompanyAccess, async (req, res) => {
   const companyId = req.query.companyId;
   const query = await company_accountants(companyId) 
+
+  res.send(query);
+});
+
+router.post("/add_company", authenticateToken, async (req, res) => {
+  const { first_name, last_name, email, phone, name_company, tin, zip_code, city, address, parent_company_id } = req.body;
+  const query = await add_company(first_name, last_name, email, phone, name_company, tin, zip_code, city, address, parent_company_id);
 
   res.send(query);
 });

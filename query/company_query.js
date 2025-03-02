f22933a42e2127c98b7684fad62eca86b90b6bdb
Maciej Rościszewski <maciej.rosciszewski@progrupa.com>
2025-03-02 20:47:09 +0100

    Edit response

--- query/company_query.js
+++ query/company_query.js
@@ -93,7 +93,7 @@ export const add_company = async (first_name, last_name, email, phone, name_comp
 
     const idCompany = resultCompany.insertId;
 
-    const resultUser = await connection.query(
+    const [resultUser] = await connection.query(
       `INSERT INTO users (company_id, first_name, last_name, email, password, theme, phone_number, address_street, address_city, address_postal_code, user_role, bookkeeping, employment_date, user_status, contract_type, vacation_days, medical_exam_date, hourly_rate, monthly_rate, is_verified, verification_token, created_at, updated_at) VALUES (?, ?, ?, ?, NULL, "defaultTheme", ?, NULL, NULL, NULL, "manager", false, NULL, true, NULL, NULL, NULL, NULL, NULL, false, NULL, NOW(), NOW())`,
       [idCompany, first_name, last_name, email, phone]);
 
@@ -103,7 +103,7 @@ export const add_company = async (first_name, last_name, email, phone, name_comp
     return {
       message: "server.add_contractors.add_child_company",
       error: false,
-      data: resultUser
+      data: null
     }
   } catch (error) {
     await connection.rollback();

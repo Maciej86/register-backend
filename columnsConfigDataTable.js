8a9562f6e46b7a85ee72fe88be2a01c0e84031d6
Maciej Rościszewski <maciej.rosciszewski@progrupa.com>
2025-03-12 14:59:25 +0100

    Fix name

--- columnsConfigDataTable.js
+++ columnsConfigDataTable.js
@@ -1,7 +1,7 @@
 export const columnsConfigDataTable = (columns, selectedColumns) => {
   return columns.filter(column => selectedColumns.includes(column.field)).map(column => ({
     field: column.field,
-    nazwa: column.name,
+    name: column.name,
     size: column.size,
     visible: column.visible,
     textAlign: column.textAlign
